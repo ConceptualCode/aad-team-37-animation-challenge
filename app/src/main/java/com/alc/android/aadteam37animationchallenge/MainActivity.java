76c887c535c981ebcdfa5625698e0160637e44f3
ConceptualCode <soronnadianthony@gmail.com>
2019-10-20 22:35:35 +0100

    Team profile added

--- app/src/main/java/com/alc/android/aadteam37animationchallenge/MainActivity.java
+++ app/src/main/java/com/alc/android/aadteam37animationchallenge/MainActivity.java
@@ -1,14 +1,36 @@
 package com.alc.android.aadteam37animationchallenge;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView1, mTextView2, mTextView3, mTextView4, mTextView5, mTextView6,
+            mTextView7;
+
+    private ImageView mImageViewMadu, mImageViewTony, mImageViewIsaiah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//        }
     }
 }
